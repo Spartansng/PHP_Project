6377470d22a630a5ff3d1a7876e5ef61c6c41970
Sinigaglia Lorenzo <lorenzo.sinigaglia@ynov.com>
2026-03-18 14:16:10 +0100

    feat(footer): add layout for footer

---
+++ templates/layout/footer.php
@@ -0,0 +1,8 @@
+</main>
+<footer class="footer">
+    <small>TP PHP — Auth + CRUD + Admin — <?= date('Y') ?></small>
+</footer>
+<script src="/assets/js/slideshow.js"></script>
+<script src="/assets/js/achievement.js"></script>
+</body>
+</html>
