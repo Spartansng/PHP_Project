cf4b03f914aeb7dcf3fb824aab30e4946a0b245f
Sinigaglia Lorenzo <lorenzo.sinigaglia@ynov.com>
2026-03-22 18:57:17 +0100

    fix(footer.php): refactor footer style

--- templates/layout/footer.php
+++ templates/layout/footer.php
@@ -1,6 +1,6 @@
 </main>
 <footer class="footer">
-    <small>TP PHP — Auth + CRUD + Admin — <?= date('Y') ?></small>
+    <small>TP PHP — <?= date('Y') ?></small>
 </footer>
 <script src="/assets/js/slideshow.js"></script>
 <script src="/assets/js/achievement.js"></script>
